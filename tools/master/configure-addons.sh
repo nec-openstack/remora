da340e29345aae177bc7500f4704aecbb9b26de2
OTSUKA, Yuanying <yuanying@fraction.jp>
2017-03-07 16:42:57 +0900

    Refactor method to install addons

--- tools/master/configure-addons.sh
+++ tools/master/configure-addons.sh
@@ -11,13 +11,15 @@ done
 
 KUBE_ADDONS_DIR=/etc/kubernetes/addons
 
-echo "Install essential addons..."
+ADDONS="kube-proxy kube-dns kube-cni-${KUBE_CNI_PLUGIN}"
 
-cat ${KUBE_ADDONS_DIR}/kube-proxy.yaml \
-    | ${DOCKER_PATH} run --net=host --rm -i ${HYPERKUBE_IMAGE_REPO}:${KUBE_VERSION} /hyperkube kubectl apply -f -
-
-cat ${KUBE_ADDONS_DIR}/kube-dns.yaml \
-    | ${DOCKER_PATH} run --net=host --rm -i ${HYPERKUBE_IMAGE_REPO}:${KUBE_VERSION} /hyperkube kubectl apply -f -
-
-cat ${KUBE_ADDONS_DIR}/kube-cni-${KUBE_CNI_PLUGIN}.yaml \
-    | ${DOCKER_PATH} run --net=host --rm -i ${HYPERKUBE_IMAGE_REPO}:${KUBE_VERSION} /hyperkube kubectl apply -f -
+echo "Install addons..."
+for ADDON in ${ADDONS}; do
+    echo "Addon: ${ADDON}"
+    cat ${KUBE_ADDONS_DIR}/${ADDON}.yaml \
+        | ${DOCKER_PATH} run \
+            --net=host \
+            --rm -i \
+            ${HYPERKUBE_IMAGE_REPO}:${KUBE_VERSION} \
+            /hyperkube kubectl apply -f -
+done
