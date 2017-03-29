6c61b8a414f1d54fab642e0299b3495807758844
OTSUKA, Yuanying <yuanying@fraction.jp>
2017-03-29 11:45:54 +0900

    Improve keepalived support

--- tools/install-keepalived.sh
+++ tools/install-keepalived.sh
@@ -7,7 +7,11 @@ address_pattern=${1:-".*"}
 ROOT=$(dirname "${BASH_SOURCE}")
 source ${ROOT}/default-env.sh
 
-echo 'Please setup correct ${NODE_NETWORK_RANGE} and ${NODE_NET_DEVICE} variables.'
+echo
+echo '**** Please setup valid following variables...'
+echo '    -   ${NODE_NETWORK_RANGE}'
+echo '    -   ${NODE_NET_DEVICE}'
+echo
 
 for MASTER in ${MASTERS}; do
     if [[ ! ${MASTER} =~ ${address_pattern} ]]; then
@@ -15,4 +19,6 @@ for MASTER in ${MASTERS}; do
     fi
     echo "Install keepalived to Master Node: ${MASTER}"
     source ${ROOT}/configure-node.sh keepalived ${MASTER}
+    echo "Install HAProxy to Master Node: ${MASTER}"
+    source ${ROOT}/configure-node.sh lb ${LB}
 done
