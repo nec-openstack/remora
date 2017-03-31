78699fd84a627fa966d8f398f80d2af32943efb5
OTSUKA, Yuanying <yuanying@fraction.jp>
2017-03-31 16:29:51 +0900

    Add node-exporter-collectors

---
+++ tools/node-exporter-collectors/configure.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+export NODE_IP=${1:-${NODE_IP}}
+
+set -eu
+export LC_ALL=C
+
+ROOT=$(dirname "${BASH_SOURCE}")
+source ${ROOT}/default-env.sh
+
+SMARMON_SERVICE=/etc/systemd/system/node-exporter-smartmon.service
+cat <<EOF > ${SMARMON_SERVICE}
+[Unit]
+Description=Collect smart data to /var/lib/node-exporter
+After=docker.service
+Requires=docker.service
+
+[Service]
+Type=oneshot
+ExecStartPre=-${DOCKER_PATH} stop node-exporter-smartmon
+ExecStartPre=-${DOCKER_PATH} rm node-exporter-smartmon
+ExecStartPre=${DOCKER_PATH} pull yuanying/node-exporter-smartmon:latest
+ExecStart=${DOCKER_PATH} run --rm --name node-exporter-smartmon \
+    --privileged > /var/lib/node-exporter/smartmon.prom
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+SMARMON_TIMER=/etc/systemd/system/node-exporter-smartmon.timer
+cat <<EOF > ${SMARMON_TIMER}
+[Unit]
+Description=Run node-exporter-smartmon service every 10 minutes
+
+[Timer]
+OnCalendar=*:0/10
+EOF
+
+systemctl daemon-reload
+systemctl enable node-exporter-smartmon
+systemctl enable node-exporter-smartmon.timer
+systemctl restart node-exporter-smartmon
+systemctl restart node-exporter-smartmon.timer
