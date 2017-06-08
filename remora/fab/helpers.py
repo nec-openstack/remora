49df5573e986adae5f7b77165923c14fd54941ea
OTSUKA, Yuanying <yuanying@fraction.jp>
2017-06-08 15:38:09 +0900

    Fix merge strategy

--- remora/fab/helpers.py
+++ remora/fab/helpers.py
@@ -42,7 +42,7 @@ def recreate_remote_temp_dir(target):
 def merge_dicts(dict1, dict2):
     """ Recursively merges dict2 into dict1 """
     if not isinstance(dict1, dict) or not isinstance(dict2, dict):
-        return dict2
+        return dict1
     for k in dict2:
         if k in dict1:
             dict1[k] = merge_dicts(dict1[k], dict2[k])
