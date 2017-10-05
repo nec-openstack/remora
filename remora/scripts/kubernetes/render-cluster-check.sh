25a33d996b8505fe879eef144fa59fa152387fed
OTSUKA, Yuanying <yuanying@fraction.jp>
2017-10-05 18:20:42 +0900

    Fix cluster-check.sh

--- remora/scripts/kubernetes/render-cluster-check.sh
+++ remora/scripts/kubernetes/render-cluster-check.sh
@@ -12,28 +12,18 @@ export LC_ALL=C
 ROOT=$(dirname "${BASH_SOURCE}")
 
 CLUSTER_STATUS='pending'
-while [ \${CLUSTER_STATUS} = 'pending' ]; do
+while [[ \${CLUSTER_STATUS} != 'Running' ]]; do
   template="{{range .items}}{{.status.phase}}{{\\"\\n\\"}}{{end}}"
   ${LOCAL_KUBECTL} \\
     -n kube-system \\
     --kubeconfig ${KUBE_ASSETS_DIR}/kubeconfig \\
     get pod -l "tier==control-plane"
-  result_status=\$(${LOCAL_KUBECTL} \\
+  CLUSTER_STATUS=\$(${LOCAL_KUBECTL} \\
     -n kube-system \\
     --kubeconfig ${KUBE_ASSETS_DIR}/kubeconfig \\
     get pod -l "tier==control-plane" -o go-template="\${template}" \\
     | sort | uniq \\
   )
-  echo \${result_status}
-  count=\$(echo \${result_status} | wc -l)
-  if [ \${count} = 1 ] ; then
-    if [ \${result_status} = 'Running' ] ; then
-      CLUSTER_STATUS='Running'
-    fi
-  else
-    echo "Checking..."
-    sleep 5
-  fi
 done
 
 echo "All control-planes are running"
