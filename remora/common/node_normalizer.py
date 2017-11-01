b86dd8474e106e951b52d9815c9abfd4a2e22ce0
OTSUKA, Yuanying <yuanying@fraction.jp>
2017-11-01 12:28:59 +0900

    Implements node_normalizer

---
+++ remora/common/node_normalizer.py
@@ -0,0 +1,76 @@
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import logging
+
+from remora import constants
+from remora.fab import helpers
+
+
+LOG = logging.getLogger(__name__)
+
+
+def _merge_config(user_config):
+    """Merge default.yaml and user inputed config"""
+    # FIXME(yuanying): use improved merge_dicts
+    merged_config = helpers.merge_dicts(
+        user_config,
+        constants.DEFAULT_CONFIG
+    )
+    return merged_config
+
+
+def _extract_nodes_from_node_groups(node_groups, default_spec={}):
+    node_list = {}
+    for _, node_group in node_groups.items():
+        node_spec = node_group.get('spec', {})
+        # FIXME(yuanying): use improved merge_dicts
+        spec = helpers.merge_dicts(node_spec, default_spec)
+        nodes = node_group.get('nodes', [])
+        for node in nodes:
+            if node in node_list.keys():
+                LOG.warn("node: {} is already defined".format(node))
+            else:
+                node_list[node] = spec
+    return node_list
+
+
+def normarize(user_config):
+    """Return nodes dict
+
+    ex:
+        {
+            'node_ip_or_name01': {
+                'kubernetes': {},
+                'etcd': {},
+            },
+            'node_ip_or_name2': {
+                'kubernetes': {},
+                'etcd': {},
+            }
+        }
+    """
+    user_config = _merge_config(user_config)
+    default_spec = user_config['spec']
+    node_groups = user_config['nodeGroups']
+    nodes = _extract_nodes_from_node_groups(node_groups, default_spec)
+
+    return nodes
+
+
+def calculated_nodes(user_config):
+    """
+    Return nodes dict which contains calculated values
+    such as etcd server list
+    """
+    return normarize(user_config)
---
+++ remora/tests/unit/common/test_node_normarizer.py
@@ -0,0 +1,91 @@
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import copy
+import os
+
+from remora.common import node_normalizer
+from remora import constants
+import remora.tests.unit.base as base
+
+
+class TestMergeConfig(base.TestCase):
+
+    def test_return_default_yaml(self):
+        self.assertEqual(
+            constants.DEFAULT_CONFIG,
+            node_normalizer._merge_config({})
+        )
+
+    def test_return_merged_config(self):
+        input_config = {"spec": {"hoge": "fugafuga"}}
+        expected = copy.deepcopy(constants.DEFAULT_CONFIG)
+        expected['spec']['hoge'] = 'fugafuga'
+
+        self.assertEqual(
+            expected,
+            node_normalizer._merge_config(input_config)
+        )
+
+
+class TestExtractNodesFromNodeGroups(base.TestCase):
+
+    def test_dict_contains_node_name_as_keys(self):
+        node_groups = {
+            'master': {
+                'nodes': ["a", "b"]
+            },
+            'worker': {
+                'nodes': ["c", "d"]
+            }
+        }
+        expected = set(["a", "b", "c", "d"])
+
+        output = node_normalizer._extract_nodes_from_node_groups(node_groups)
+        self.assertEqual(
+            expected,
+            set(output.keys())
+        )
+
+    def test_dict_contains_node_groups_spec(self):
+        expected_spec = { 'expected': 'hoge' }
+        node_groups = {
+            'master': {
+                'spec': expected_spec,
+                'nodes': ["a", "b"]
+            }
+        }
+
+        output = node_normalizer._extract_nodes_from_node_groups(node_groups)
+        for v in output.values():
+            self.assertEqual(expected_spec, v)
+
+    def test_dict_contains_default_spec(self):
+        default_spec = { 'default': 'spec' }
+        node_spec = { 'node': 'spec' }
+        expected_spec = {
+            'default': 'spec',
+            'node': 'spec'
+        }
+        node_groups = {
+            'master': {
+                'spec': node_spec,
+                'nodes': ["a", "b"]
+            }
+        }
+        output = node_normalizer._extract_nodes_from_node_groups(
+            node_groups,
+            default_spec
+        )
+        for v in output.values():
+            self.assertEqual(expected_spec, v)
