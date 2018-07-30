97bb5dad776aa41c3f076f2b14183c48a038cdd2
OTSUKA, Yuanying <yuanying@fraction.jp>
2018-07-30 09:25:43 +0900

    Remove /var/lib/kubelet on cleaning

--- remora/fab/clean/__init__.py
+++ remora/fab/clean/__init__.py
@@ -31,7 +31,6 @@ def dependency():
     require('stage')
     with settings(warn_only=True):
         sudo('rm -rf {0}'.format(env['configs']['temp_dir']))
-        sudo('rm -rf /var/lib/kubelet')
         if env['configs'].get('clean', {}).get('mode', 'soft') == 'soft':
             sudo('docker rm -f `docker ps -a -q`')
         else:
@@ -39,6 +38,7 @@ def dependency():
             sudo('rm -rf /var/lib/docker')
             sudo('systemctl daemon-reload && systemctl enable docker')
 
+        sudo('rm -rf /var/lib/kubelet')
         reboot()
 
 
